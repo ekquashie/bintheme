36454e06151afd7f966230c82cce59eeb1b63512
Eyram Quashie <eyramkobla.quashie@gmail.com>
2021-08-13 02:39:49 +0000

    Wordpress e-commerce theme

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+
+		<div class="top-header">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-5">
+						<a href="/wpdev">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo">
+						</a>
+					</div>
+					<div class="col-lg-7`">
+						<div class="top-header-contact">
+							<a href="tel:0548482872"><i class="fa fa-phone"></i>+233(0) 548 482 872</a>
+							<a href="mailto: quashieeyram@gmail.com"><i class="fa fa-envelope-o"></i>quashieeyram@gmail.com</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+
+		<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark" aria-labelledby="main-nav-label">
+
+			<div class="container">
+
+				<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button> -->
+
+				<!-- The WordPress Menu goes here -->
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				);
+				?>
+			</div><!-- .container -->
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
